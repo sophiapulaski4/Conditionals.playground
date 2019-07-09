7867a7fd56892de301524036c7f38ee9abc056a5
caroline dugan <caroline.dugan@gmail.com>
2019-07-09 13:25:15 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+//true or false
+//print(num1<num2)
+//print(num1>=num2)
+
+//print(num1==num2)
+//
+//
+//
+////checking if num1 is not equal to num2
+////true
+//print(num1 != num2)
+
+//if num1+num2 == num3 && num2+num1 == num3 {
+//// then do something
+//print("yes!")
+//}
+
+//// checking if one equals 2, since second part is true, now yes! prints
+//if num1 == num2 || num1 == num1 {
+//    print("yes!")
+//}
+
+//"Karlie" == "karlie" //false
+
+
+
+var dogAge = 1
+//// if this is true
+//if dogAge > 10 {
+//    // then i do this
+//print ("old doggie!)
+//}
+
+//if dogAge < 2 {
+//    print("You are a puppy")
+//}
+//
+//if dogAge == 3 {
+//    print("you are awesome")
+//}
+//
+//if dogAge > 12 {
+//    print("you are elderly!")
+//} else if dogAge < 10{
+//    print("Young Puppy")
+//}
+var FavoriteFood = "Chipotle"
+if "Chipotle" == "Chipotle"{
+    print("😇💫🌈🛸🚀💘💞")
+}
+
+
+
